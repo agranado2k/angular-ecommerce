b95b74b76aaee2f3638ce80aadcb05bb71c42b6f
Arthur Granado <agranado@pluga.co>
2017-11-14 20:04:35 -0200

    Include libs ng-bootstrap, firebase and angularfire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng2BootstrapModule } from 'ng-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShoppingCartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2BootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

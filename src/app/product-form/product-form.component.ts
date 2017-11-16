df3b2f3d97a10c64b11a2327c32d02ef4ac9530c
Arthur Granado <agranado@pluga.co>
2017-11-16 00:16:29 -0200

    Part IIIe

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../services/product.service';
 import { CategoryService } from './../services/category.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -9,15 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
+  product = {};
+  id;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private productService: ProductService) {
     this.categories$ = categoryService.getAll();
+
+    this.id = route.snapshot.paramMap.get('id');
+    if (this.id) productService.get(this.id).subscribe(p => this.product = p);
   }
 
   ngOnInit() {
   }
 
   save(product) {
-    this.productService.save(product);
+    if (this.id)
+      this.productService.update(this.id, product);
+    else
+      this.productService.save(product);
+
+    this.router.navigateByUrl('/admin/products');
+  }
+
+  delete() {
+    if (!confirm('Are you sure that you want to delete this product?')) return;
+
+    this.productService.delete(this.id);
+    this.router.navigateByUrl('/admin/products');
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,4 +16,18 @@ export class ProductService {
         return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
       });
   }
+
+  get(id) {
+    return this.afDb.object(`/products/${id}`)
+      .snapshotChanges()
+      .map(c => ({ key: c.payload.key, ...c.payload.val() }));
+  }
+
+  update(id, product) {
+    return this.afDb.object(`/products/${id}`).update(product);
+  }
+
+  delete(id) {
+    return this.afDb.object(`/products/${id}`).remove();
+  }
 }
